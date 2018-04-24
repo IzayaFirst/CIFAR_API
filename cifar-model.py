173ee5840233b0ca93d1cdd0e95f579f85246b65
patcharaleelalumplert@gmail.com <patcharaleelalumplert@gmail.com>
2018-04-25 01:10:10 +0700

    save result

--- cifar-model.py
+++ cifar-model.py
@@ -4,6 +4,7 @@ Original dataset and info: https://www.cs.toronto.edu/~kriz/cifar.html for more 
 '''
 from __future__ import print_function
 
+
 import numpy as np
 
 from keras.callbacks import EarlyStopping
@@ -25,18 +26,18 @@ if __name__ == '__main__':
     # Create the model
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
+    model.summary()
 
     # Compile the model
     model.compile(loss='categorical_crossentropy',
@@ -47,13 +48,12 @@ if __name__ == '__main__':
     model.fit(X_train / 255.0, to_categorical(Y_train),
               batch_size=128,
               shuffle=True,
-              epochs=250,
+              epochs=100,
               validation_data=(X_test / 255.0, to_categorical(Y_test)),
               callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
 
     # Evaluate the model
     scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
-
     print('Loss: %.3f' % scores[0])
     print('Accuracy: %.3f' % scores[1])
     model.save('cifar10.h5')
